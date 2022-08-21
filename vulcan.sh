d58c6afcacadacae1d5cc32f73f8a769bf872626
ricardo-f <riclima1990@gmail.com>
2022-08-21 11:21:47 -0300

    Adding functions to create pipeline files

--- vulcan.sh
+++ vulcan.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -eo pipefail
 
 PRJ_ENVS=("prod" "dev")
 TF_FILES=("main" "data" "variables" "outputs")
@@ -17,17 +18,49 @@ f_create_env_files() {
   for env in ${PRJ_ENVS[@]}; do
     touch $env.tfvars
   done
-  cd ..
+  cd ../..
+}
+
+f_gha_files() {
+  GHA_FILES=("ci" "cd")
+  mkdir -p .github/workflows
+  cd .github/workflows
+  for name in ${GHA_FILES[@]}; do
+    touch $name.yaml
+  done
+  cd ../..
+}
+
+f_bitbucket_file() {
+  touch bitbucket-pipelines.yml
+}
+
+f_jenkinsfile() {
+  touch Jenkinsfile
 }
 
 case $1 in
-basic)
-  f_create_tf_basic_files
-  echo "Created basic terraform file"
-  ;;
-basicEnv)
-  f_create_tf_basic_files
-  f_create_env_files
-  echo "Created basic terraform file with tfvars"
-  ;;
+  basic)
+    f_create_tf_basic_files
+    echo "Created basic terraform file"
+    ;;
+  basicEnv)
+    f_create_tf_basic_files
+    f_create_env_files
+    echo "Created basic terraform file with tfvars"
+    ;;
+esac
+
+case $2 in
+  githubActions)
+    f_gha_files
+    ;;
+  bitbucket)
+    f_bitbucket_file
+    ;;
+  jenkins)
+    f_jenkinsfile
+    ;;
+  *)
+    ;;
 esac
