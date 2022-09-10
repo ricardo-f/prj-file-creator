84bedbb27dca8994f8ff7e865a4b824425166941
ricardo-f <riclima1990@gmail.com>
2022-09-10 10:59:38 -0300

    adding func to create nodeJs structure

--- vulcan.sh
+++ vulcan.sh
@@ -5,62 +5,80 @@ PRJ_ENVS=("prod" "dev")
 TF_FILES=("main" "data" "variables" "outputs")
 
 f_create_tf_basic_files() {
-  mkdir terraform
-  cd terraform
-  for file in ${TF_FILES[@]}; do
-    touch $file.tf
-  done
+    mkdir terraform
+    cd terraform
+    for file in ${TF_FILES[@]}; do
+        touch $file.tf
+    done
 }
 
 f_create_env_files() {
-  mkdir envs
-  cd envs
-  for env in ${PRJ_ENVS[@]}; do
-    touch $env.tfvars
-  done
-  cd ../..
+    mkdir envs
+    cd envs
+    for env in ${PRJ_ENVS[@]}; do
+        touch $env.tfvars
+    done
+    cd ../..
+}
+
+f_create_nodejs_files() {
+    mkdir src
+    cd src
+    mkdir models views controllers routes
+    touch index.js
+    cd ..
 }
 
 f_gha_files() {
-  GHA_FILES=("ci" "cd")
-  mkdir -p .github/workflows
-  cd .github/workflows
-  for name in ${GHA_FILES[@]}; do
-    touch $name.yaml
-  done
-  cd ../..
+    GHA_FILES=("ci" "cd")
+    mkdir -p .github/workflows
+    cd .github/workflows
+    for name in ${GHA_FILES[@]}; do
+        touch $name.yaml
+    done
+    cd ../..
 }
 
 f_bitbucket_file() {
-  touch bitbucket-pipelines.yml
+    touch bitbucket-pipelines.yml
 }
 
 f_jenkinsfile() {
-  touch Jenkinsfile
+    touch Jenkinsfile
 }
 
 case $1 in
-  basic)
-    f_create_tf_basic_files
-    echo "Created basic terraform file"
+    basic)
+        f_create_tf_basic_files
+        echo "Created basic terraform file"
     ;;
-  basicEnv)
-    f_create_tf_basic_files
-    f_create_env_files
-    echo "Created basic terraform file with tfvars"
+    basicEnv)
+        f_create_tf_basic_files
+        f_create_env_files
+        echo "Created basic terraform file with tfvars"
+    ;;
+    nodeApp)
+        f_create_nodejs_files
+        echo "Created basic nodejs files based on MVC"
+    ;;
+    nodeAppEac)
+        f_create_tf_basic_files
+        f_create_env_files
+        f_create_nodejs_files
+        echo "Created basic EaC project for NodeJs app"
     ;;
 esac
 
 case $2 in
-  githubActions)
-    f_gha_files
+    githubActions)
+        f_gha_files
     ;;
-  bitbucket)
-    f_bitbucket_file
+    bitbucket)
+        f_bitbucket_file
     ;;
-  jenkins)
-    f_jenkinsfile
+    jenkins)
+        f_jenkinsfile
     ;;
-  *)
+    *)
     ;;
 esac
